89af069cfedc4feeb9a67cf65c8319851a9d004d
Diogo Alves <diogodidi2022@gmail.com>
2023-01-15 22:17:40 -0300

    demonstrating how to place an environment variable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: { // para acessar a variavel de ambiente caminho: process.env.bdPath
+    bdPath: "mysql..."
+  }
 }
 
 module.exports = nextConfig
