08cd7f76e0e08e0df1cfc31fb7f0ea9dc84576a4
Diogo Alves <diogodidi2022@gmail.com>
2023-01-16 18:36:12 -0300

    adding new file

--- src/pages/rotas/index.jsx
+++ src/pages/rotas/index.jsx
@@ -1,3 +1,20 @@
+import Link from "next/link";
+
 export default function rotas() {
-    return <h1>Rotas index</h1>
+    return (
+        <div>
+            <h1>Rotas index</h1>
+            <ul>
+                <Link href="/rotas/params?id=12&nome=Ana">
+                    <li>Params</li>
+                </Link>
+                <Link href="/rotas/123/buscar">
+                    <li>Params</li>
+                </Link>
+                <Link href="/rotas/123/Daniel">
+                    <li>Params</li>
+                </Link>
+            </ul>
+        </div>
+    )
 }
