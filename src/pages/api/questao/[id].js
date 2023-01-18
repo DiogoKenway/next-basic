c21a22334052e68e5f9403895fa496b925dee3e8
Diogo Alves <diogodidi2022@gmail.com>
2023-01-18 01:16:03 -0300

    modfy file

--- src/pages/api/questao/[id].js
+++ src/pages/api/questao/[id].js
@@ -1,18 +1,16 @@
 export default function questao(req, res) {
     if (req.method === "GET") {
-        get(req, res)
+        const id = req.query.id
+        res.status(200).json({
+            id,
+            enunciado: "Qual a sua cor preferida?",
+            respostas: [
+                "Branco", "Vermelho", "Amarelo", "Verde"
+            ]
+        });
     } else {
         res.status(405).send();
     }
 }
 
-function get(req, res) {
-    const id = req.query.id
-    return res.status(200).json({
-        id,
-        enunciado: "Qual a sua cor preferida?",
-        respostas: [
-            "Branco", "Vermelho", "Amarelo", "Verde"
-        ]
-    });
-}
+
